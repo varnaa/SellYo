3f0617a2d451d56d7c962a33f409620fb663c79a
Swethavarnaa <swethavarnaa@gmail.com>
2021-06-29 15:57:56 +0530

    initial commit

---
+++ sellyo/src/main/java/com/varnaa/sellyo/sellyo/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.varnaa.sellyo.sellyo;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
